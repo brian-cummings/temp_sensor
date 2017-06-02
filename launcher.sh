c0985800c752b083c03eb43ac766834f6db54021
Brian Cummings <brian.cummings@careerbuilder.com>
2017-06-02 11:34:48 -0400

    Updated startup to wait for network

--- launcher.sh
+++ launcher.sh
@@ -4,5 +4,9 @@
 
 cd /
 cd home/pi/python/temp_sensor
+# Wait for network to be up (pinging Google)
+while ! ping -c 1 -W 1 8.8.8.8; do
+    sleep 1
+done
 python send_ip.py
 python3 temp_sensor.py
--- send_ip.py
+++ send_ip.py
@@ -1,5 +1,12 @@
 import get_ip
 import send_email
+import sys
+
 
 ip = get_ip.get_ipv4('wlan0')
-send_email.send_ip(ip)
+try:
+    status = send_email.send_ip(ip)
+except:
+    status = sys.exc_info()[0]
+
+
