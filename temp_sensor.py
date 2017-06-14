f02bef2ac341d212b996a53d2e785120e214b795
Brian Cummings <brian.m.cummings@gmail.com>
2017-06-14 10:56:29 -0400

    removed comments to make more readable

--- temp_sensor.py
+++ temp_sensor.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python
 
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import Adafruit_DHT, logging.handlers, os, time, google_connector
 
 file_path = os.getcwd()
@@ -43,10 +23,6 @@ while(True):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     temperature = temperature * 1.8 + 32
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
 
     if humidity is not None and temperature is not None:
         message = 'Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature, humidity)
